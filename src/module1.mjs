b1389f44acf287f2443526c1626f3b584346604b
jayesh-backend <jayeshgupta+88@garageplug.com>
2025-08-29 18:27:54 +0530

    added react project

---
+++ src/module1.mjs
@@ -0,0 +1,11 @@
+const a = 'Jayesh Gupta';
+const b = 'Ayush Gupta';
+
+const c = 'Sangeeta Gupta';
+
+const d = 'Rajkumar Gupta';
+
+const e = 'Ankit Gupta';
+
+export default b;
+export {a,b,c,d,e}
