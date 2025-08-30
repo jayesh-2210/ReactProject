9de975358e9f3d4d7a3a246f2bcf260020887597
jayesh-backend <jayeshgupta+88@garageplug.com>
2025-08-30 13:02:27 +0530

    master

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -13,10 +13,16 @@ export default function TextForm(props) {
     const [text, setText] = useState("Enter Text Here");
     // setText("new Text");
   return (
+    <>
     <div className="mb-3">
       <h1>{props.heading}</h1>
       <textarea className="form-control" id="myBox" rows="8" value={text} onChange={handleOnChange}></textarea>
       <button className="btn btn-primary my-3" onClick={handleUpperClick}>convert to uppercase</button>
     </div>
+    <div className="container">
+        <h1>Your Text Summary</h1>
+        <p>{text.split(" ").length} words and {text.length} characters</p>
+    </div>
+    </>
   );
 }
