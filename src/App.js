03f25b443e562c12137591d77a0a87a075305b5b
jayesh-backend <jayeshgupta+88@garageplug.com>
2025-09-07 21:35:38 +0530

    added about.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import About from "./components/About";
 import NavBar from "./components/NavBar";
 import TextForm from "./components/TextForm";
 
@@ -7,8 +8,8 @@ function App() {
     <>
       <NavBar title = "TextUtils" aboutText = "About us"/>
       <div className="container my-3">
-      <TextForm heading="Enter the text to Analyse Below"/>
-
+      {/* <TextForm heading="Enter the text to Analyse Below"/> */}
+      <About/>
       </div>
 
     </>
